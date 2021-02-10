4fcafa24441641b9806d5ce10523b2b29b3b511d
WhiteCrow <whitecrow@WhiteCrowui-MacBookPro.local>
2021-02-10 17:17:28 +0900

    변수 인라인하기

--- refactoring/refactoring.js
+++ refactoring/refactoring.js
@@ -10,7 +10,7 @@ function statement(invoices, plays) {
     }).format;
 
     for(let perf of invoice.performances) {
-        const play = plays[perf.playID];
+        const play = playFor(perf);
         
         let thisAmount = amountFor(perf, play);
 
@@ -30,23 +30,23 @@ function statement(invoices, plays) {
 
 }
 
-function amountFor(perf, play) {
+function amountFor(aPerformance, play) {
 
     let result = 0;
 
     switch(play.type) {
         case 'tragedy':
             result = 40000;
-            if(perf.audience > 30) {
-                result += 1000 * (perf.audience - 30);
+            if(aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
             }
             break;
         case 'comedy':
             result = 30000;
-            if(perf.audience > 20) {
-                result += 10000 + 500 * (perf.audience - 20);
+            if(aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
             }
-            result += 300 * perf.audience;
+            result += 300 * aPerformance.audience;
             break;
         default:
             throw new Error(`알 수 없는 장르: ${play.type}`);
@@ -54,6 +54,10 @@ function amountFor(perf, play) {
 
     return result;
 
+}
+
+function playFor(aPerformance) {
+    return plays[aPerformance.playID];
 }
 
 const invoices = 
