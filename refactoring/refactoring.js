bd41a8b3d003c3fcf03fa722eca762ecfecd5b3d
WhiteCrow <whitecrow@WhiteCrowui-MacBookPro.local>
2021-02-13 12:47:52 +0900

    중간 점검

--- refactoring/refactoring.js
+++ refactoring/refactoring.js
@@ -17,12 +17,12 @@ function statement(invoice, plays) {
 
 function totalAmount() {
     
-    let totalAmount = 0;
+    let result = 0;
     for(let perf of invoice.performances) {
-        totalAmount += amountFor(perf);
+        result += amountFor(perf);
     }
 
-    return totalAmount;
+    return result;
 }
 
 function totalVolumeCredits() {
