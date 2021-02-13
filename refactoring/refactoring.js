9a35db9bfa7d7a9c5bed017870a25c39b209d4b5
WhiteCrow <whitecrow@WhiteCrowui-MacBookPro.local>
2021-02-13 09:35:05 +0900

    format 변수 제거

--- refactoring/refactoring.js
+++ refactoring/refactoring.js
@@ -4,10 +4,6 @@ function statement(invoices, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-    const format = new Intl.NumberFormat('en-US', {
-        style: 'currency', currency: 'USD',
-        minimumFractionDigits: 2
-    }).format;
 
     for(let perf of invoice.performances) {
         
@@ -17,11 +13,21 @@ function statement(invoices, plays) {
         // 청구 내역을 출력한다.
         result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
+        
     }
 
     result += `총액: ${format(totalAmount/100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
+
+}
+
+function format(aNumber) {
+
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency', currency: 'USD',
+        minimumFractionDigits: 2
+    }).format(aNumber);
 
 }
 
