ddc190171973b51e26583fdaf488219c1ada1245
WhiteCrow <whitecrow@WhiteCrowui-MacBookPro.local>
2021-02-14 11:56:27 +0900

    중간 데이터 구조 변경

--- refactoring/refactoring.js
+++ refactoring/refactoring.js
@@ -1,47 +1,62 @@
 function statement(invoice, plays) {
     
-    return renderPlayText(invoice, plays);
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+    return renderPlayText(statementData, invoice, plays);
     
 }
 
-function renderPlayText(invoice, plays) {
+function enrichPerformance(aPerformance) {
     
-    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+    const result = Object.assign({}, aPerformance);
+    result.play = playFor(result);
+    result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
+    return result;
+
+}
+
+function renderPlayText(data, invoice, plays) {
     
-    for(let perf of invoice.performances) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
+    
+    for(let perf of data.performances) {
         
         // 청구 내역을 출력한다.
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
         
     }
     
-    result += `총액: ${usd(totalAmount())}\n`;
-    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
     return result;
 
 }
 
-function totalAmount() {
+function totalAmount(data) {
     
     let result = 0;
-    for(let perf of invoice.performances) {
-        result += amountFor(perf);
+    for(let perf of data.performances) {
+        result += perf.amount;
     }
     
     return result;
 }
 
-function totalVolumeCredits() {
+function totalVolumeCredits(data) {
     
-    let volumeCredits = 0;
-    for(let perf of invoice.performances) {
+    let result = 0;
+    for(let perf of data.performances) {
     
         // 포인트를 적립한다.
-        volumeCredits += volumeCreditsFor(perf);
+        result += perf.volumeCredits;
         
     }
 
-    return volumeCredits;
+    return result;
 
 }
 
