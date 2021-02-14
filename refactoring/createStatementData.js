292e6fb338d2c10498a8115d5c92f5ecdcee402a
WhiteCrow <whitecrow@WhiteCrowui-MacBookPro.local>
2021-02-14 14:24:53 +0900

    class add

--- refactoring/createStatementData.js
+++ refactoring/createStatementData.js
@@ -1,3 +1,10 @@
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+}
+
 function createStatementData(invoice, plays) {
 
   const result = {};
@@ -9,8 +16,9 @@ function createStatementData(invoice, plays) {
 
   function enrichPerformance(aPerformance) {
     
+    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
+    result.play = calculator.play;
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
     return result;
