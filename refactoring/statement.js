2cb64219fe9123a023bfccb2fc75e2d454664593
WhiteCrow <whitecrow@WhiteCrowui-MacBookPro.local>
2021-02-14 14:05:35 +0900

    htmlStatement add

--- refactoring/statement.js
+++ refactoring/statement.js
@@ -21,6 +21,31 @@ function renderPlayText(data, invoice, plays) {
 
 }
 
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+    for(let perf of data.performances) {
+        result += 
+            `<tr>
+                <td>${perf.play.name}</td>
+                <td>${perf.audience}</td>
+                <td>${usd(perf.amound)}</td>
+            </tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}점</em></p>\n`;
+
+    return result;
+
+}
+
 function usd(aNumber) {
 
     return new Intl.NumberFormat('en-US', {
@@ -58,4 +83,4 @@ const plays =
 
 const invoice = invoices[0];
 
-console.log(statement(invoice, plays));
+console.log(htmlStatement(invoice, plays));
