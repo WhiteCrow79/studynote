4ad2be59394bc0de92a76de376856217f31de262
unknown <love012xx@gmail.com>
2021-03-25 22:45:00 +0900

    BlockChain 예제

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,1 +1,40 @@
-console.log('Hello TypeScript');
+import * as CryptoJS from "crypto-js";
+
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+
+    static calculateBlockHash = (
+        index:number, 
+        previousHash: string, 
+        timestamp: number, 
+        data: string ): string =>
+        CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp
+    }
+}
+
+const genesisBlock: Block = new Block(0, "20202020", "", "Hello", 13456);
+
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length -1];
+
+const getNewTimeSamp = (): number => Math.round(new Date().getTime() / 1000);
